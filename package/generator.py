6543b223fdc3576a6d55cae8eca7d1521900a539
dzenan.dzafic <dzenan.dzafic@klika.ba>
2022-05-17 12:51:16 +0200

    Improvements are made to transaction generating algorithm

--- package/generator.py
+++ package/generator.py
@@ -12,8 +12,12 @@ def main():
     delimiter = str(input("Please insert your delimiter: "))
     account_no, iban_no = "", ""
     accounts, banks = [], list(Banks)
-    bank_exists = True
     faker = Faker()
+
+    designations = list(Designations)
+    designations.remove(designations[6])
+
+    print(designations[0].name)
 
     for index in range(number_of_accounts):
         
@@ -30,7 +34,7 @@ def main():
 
         sample_bank = random.sample(banks, 1)
         banks.remove(sample_bank[0])
-        accounts.append([account_no, iban_no, sample_bank[0].value])
+        accounts.append([sample_bank[0].value, account_no, iban_no, 1, 0])
             
         account_no, iban_no = "", ""
 
@@ -42,29 +46,40 @@ def main():
                 start_date = datetime(2019,1,1),
                 tzinfo = pytz.timezone("Europe/Sarajevo"))
 
+        account_index = random.choice(range(len(accounts)))
+
         designation_cat = random.choice(list(Designations))
-        if designation_cat == Designations.vehicle:
-            amount = float(round(random.uniform(1, 100), 2))
-        elif designation_cat == Designations.loan:
-            amount = float(round(random.uniform(100, 5000), 2))
-        elif designation_cat == Designations.pharmacy:
-            amount = float(round(random.uniform(1, 200), 2))
-        elif designation_cat == Designations.bank_fee:
-            amount = float(round(random.uniform(1, 50), 2))
-        elif designation_cat == Designations.atm:
-            amount = float(round(random.uniform(1, 1500), 2))
-        elif designation_cat == Designations.shopping:
-            amount = float(round(random.uniform(1, 10000), 2))
-        elif designation_cat == Designations.salary:
-            amount = float(round(random.uniform(1, 10000), 2))
-        elif designation_cat == Designations.other:
-            amount = float(round(random.uniform(1, 100), 2))
-        
-        account = random.choice(accounts)
+        if accounts[account_index][4] / accounts[account_index][3] < .7:
+            if designation_cat == Designations.vehicle:
+                amount = float(round(random.uniform(1, 100), 2))
+            elif designation_cat == Designations.loan:
+                amount = float(round(random.uniform(100, 5000), 2))
+            elif designation_cat == Designations.pharmacy:
+                amount = float(round(random.uniform(1, 200), 2))
+            elif designation_cat == Designations.bank_fee:
+                amount = float(round(random.uniform(1, 50), 2))
+            elif designation_cat == Designations.atm:
+                amount = float(round(random.uniform(1, 1500), 2))
+            elif designation_cat == Designations.shopping:
+                amount = float(round(random.uniform(1, 10000), 2))
+            elif designation_cat == Designations.salary:
+                amount = float(round(random.uniform(1, 10000), 2))
+                accounts[account_index][3] += amount
+            elif designation_cat == Designations.other:
+                amount = float(round(random.uniform(1, 100), 2))
+
+            accounts[account_index][4] += amount
+
+        else:
+            designation_cat = Designations.salary
+            amount = float(round(random.uniform(1400, 10000), 2))
+            accounts[account_index][3] += amount
+            
+
         data.append([
                 date_time.strftime("%-d.%-m.%Y %H:%M"),
                 random.choice(designation_cat.value),
-                amount, account[2], account[0], account[1]])
+                amount, accounts[account_index][0], accounts[account_index][1], accounts[account_index][2]])
 
     dataFrame = pd.DataFrame(data, columns = columns)
 
@@ -75,6 +90,9 @@ def main():
     dataFrame.to_csv(os.path.join(os.getcwd(), "exports", f"export_{ now }_accounts_{ len(accounts) }.csv"), sep = delimiter, index = False)
 
     print(dataFrame)
+
+    for i in accounts:
+        print(i)
 
 if __name__ == "__main__":
     main()
